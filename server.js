8334450cc1b23129496f93f69f1d5dd4d54eed38
Hector Garcia <joegl1991@gmail.com>
2020-06-28 18:56:46 -0500

    Primer commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express();
+const hbs = require('hbs');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+// Express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+// Helpers
+require('./hbs/helpers');
+
+app.get('/', (req, res) => {
+
+    res.render('home', {
+        nombre: 'héCtor'
+    });
+
+});
+
+app.get('/about', (req, res) => {
+
+    res.render('about');
+
+});
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`);
+});
